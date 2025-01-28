97a2f9849f39d5ae04aea705aa68521214fa153d
Mwiti Peter <mwitipet@gmail.com>
2025-01-28 21:26:27 +0300

    first edit commit

---
+++ job-portal-client/src/Pages/CreateJob.jsx
@@ -0,0 +1,206 @@
+import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+
+import CreatableSelect from "react-select/creatable";
+import Result from "postcss/lib/result";
+
+const CreateJob = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const {
+    register,
+    handleSubmit,reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    data.skills = selectedOption;
+    // console.log(data);
+    fetch("http://localhost:3000/post-job",{
+      method: "POST",
+      headers:{"Content-type" : "application/json"},
+      body: JSON.stringify(data)
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        if(result.acknowledged === true ){
+          alert("Job Posted Successfully!!!")
+        }
+        reset()
+      });
+  };
+
+  const option = [
+    { value: "JavaScript", label: "JavaScript" },
+    { value: "C++", label: "C++" },
+    { value: "HTML", label: "HTML" },
+    { value: "CSS", label: "CSS" },
+    { value: "React", label: "React" },
+    { value: "Node", label: "Node" },
+    { value: "MongoDB", label: "MongoDB" },
+    { value: "Redux", label: "Redux" },
+  ];
+
+  return (
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
+      {/* form */}
+      <div className="bg-[#FAFAFA] py-10 px-4 lg:px-16">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5  ">
+          {/* 1st row */}
+          <div className="create-job-flex">
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">Job Title</label>
+              <input
+                type="text"
+                defaultValue={"Web Developer"}
+                {...register("jobTitle")}
+                className="create-job-input"
+              />
+            </div>
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">Company Name</label>
+              <input
+                type="text"
+                placeholder="Ex: Microsoft"
+                {...register("companyName")}
+                className="create-job-input"
+              />
+            </div>
+          </div>
+          {/* 2nd row */}
+          <div className="create-job-flex">
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">Minimum Salary</label>
+              <input
+                type="text"
+                placeholder="$20k"
+                {...register("minPrice")}
+                className="create-job-input"
+              />
+            </div>
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">Maximum Salary</label>
+              <input
+                type="text"
+                placeholder="$120k"
+                {...register("maxPrice")}
+                className="create-job-input"
+              />
+            </div>
+          </div>
+          {/* 3rd row */}
+          <div className="create-job-flex">
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">Salary Type</label>
+              <select {...register("salaryType")} className="create-job-input">
+                <option value="">Choose your salary</option>
+                <option value="Hourly">Hourly</option>
+                <option value="Monthly">Monthly</option>
+                <option value="Yearly">Yearly</option>
+              </select>
+            </div>
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">jobLocation</label>
+              <input
+                type="text"
+                placeholder="Ex: New York"
+                {...register("jobLocation")}
+                className="create-job-input"
+              />
+            </div>
+          </div>
+
+          {/* 4th row */}
+          <div className="create-job-flex">
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">job posting date</label>
+              <input
+                type="date"
+                placeholder="Ex: 2024-03-31"
+                {...register("postingDate")}
+                className="create-job-input"
+              />
+            </div>
+
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">Experience Level</label>
+              <select
+                {...register("experienceLevel")}
+                className="create-job-input"
+              >
+                <option value="">Choose your experience</option>
+                <option value="NoExperience">NoExperience</option>
+                <option value="Internship">Internship</option>
+                <option value="Work remotely">Work remotely</option>
+              </select>
+            </div>
+          </div>
+          {/* 5th row */}
+          <div>
+            <label className="block mb-2 text-lg">Required Skill Set:</label>
+            <CreatableSelect
+              defaultValue={selectedOption}
+              onChange={setSelectedOption}
+              options={option}
+              isMulti
+              className="create-job-input py-4"
+            />
+          </div>
+          {/* 6th row */}
+          <div className="create-job-flex">
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">Company Logo</label>
+              <input
+                type="url"
+                placeholder="Paste you company logo URL: https://wetransfer.com"
+                {...register("companyLogo")}
+                className="create-job-input"
+              />
+            </div>
+
+            <div className="lg:w-1/2 w-full">
+              <label className="block mb-2 text-lg">Employment Type</label>
+              <select
+                {...register("employmentType")}
+                className="create-job-input"
+              >
+                <option value="">Choose your Type of Employment</option>
+                <option value="Full-time">Full-time</option>
+                <option value="Part-time">Part-time</option>
+                <option value="Temporary">Temporary</option>
+              </select>
+            </div>
+          </div>
+
+          {/* 7th row */}
+          <div className="w-full">
+            <label className="block mb-2 text-lg">Job Decription</label>
+            <textarea
+              className="w-full pl-3 py-1.5 focus:outline-none placeholder:text-gray-700"
+              rows={6}
+              defaultValue={" You can get a job only if you qualify ...."}
+              placeholder="Job description"
+              {...register("description")}
+            />
+          </div>
+          {/* Last  row */}
+          <div className="w-full">
+            <label className="block mb-2 text-lg">Job Posted By</label>
+            <input
+              type="email"
+              placeholder="your email"
+              {...register("postedBy")}
+              className="create-job-input"
+            />
+          </div>
+          <input
+            type="submit"
+            className="block mt-12 bg-blue text-white font-semibold px-8 py-2 rounded-sm cursor-pointer"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateJob;
---
+++ job-portal-client/src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbtsUEEXSUZ0aEClR2-4kw3ur_v1F741M",
+  authDomain: "riorgroup-5a186.firebaseapp.com",
+  projectId: "riorgroup-5a186",
+  storageBucket: "riorgroup-5a186.appspot.com",
+  messagingSenderId: "1019704356418",
+  appId: "1:1019704356418:web:9bb5683dba01680f29c7db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
